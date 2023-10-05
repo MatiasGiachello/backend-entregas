419a1a76d0dd8c864bc7f916469b3420b7246d1e
Matias Giachello <matiasgiachello04@gmail.com>
2023-10-05 20:06:13 -0300

    Primera Entrega

--- desafio3/ProductManager.js
+++ desafio3/ProductManager.js
@@ -1,109 +1,109 @@
-import fs from 'fs'
+// import fs from 'fs'
 
-class ProductManager {
-    constructor(path) {
-        this.products = []
-        this.path = path
-    }
+// class ProductManager {
+//     constructor(path) {
+//         this.products = []
+//         this.path = path
+//     }
 
-    writeFile = async data => {
-        try {
-            await fs.promises.writeFile(
-                this.path, JSON.stringify(data, null, 2)
-            )
-        } catch (error) {
-            console.log(error)
-        }
-    }
+//     writeFile = async data => {
+//         try {
+//             await fs.promises.writeFile(
+//                 this.path, JSON.stringify(data, null, 2)
+//             )
+//         } catch (error) {
+//             console.log(error)
+//         }
+//     }
 
-    getProducts = async () => {
-        try {
-            const productsList = await fs.promises.readFile(this.path, 'utf-8')
-            const product = productsList === "" ? [] : JSON.parse(productsList)
-            return product
-        } catch (error) {
-            if (error.message.includes('ENOENT: no such file or directory')) return []
-            console.log(error)
-        }
-    }
+//     getProducts = async () => {
+//         try {
+//             const productsList = await fs.promises.readFile(this.path, 'utf-8')
+//             const product = productsList === "" ? [] : JSON.parse(productsList)
+//             return product
+//         } catch (error) {
+//             if (error.message.includes('ENOENT: no such file or directory')) return []
+//             console.log(error)
+//         }
+//     }
 
-    addProduct = async (title, description, price, thumbnail, code, stock) => {
+//     addProduct = async (title, description, price, thumbnail, code, stock) => {
 
-        let products = await this.getProducts()
+//         let products = await this.getProducts()
 
-        const product = {
-            title,
-            description,
-            price,
-            thumbnail,
-            code,
-            stock
-        }
+//         const product = {
+//             title,
+//             description,
+//             price,
+//             thumbnail,
+//             code,
+//             stock
+//         }
 
-        if (!title || !description || !price || !thumbnail || !code || !stock) {
-            console.log("Debe completar todos los Datos para Generar un nuevo Producto")
-            return
-        }
+//         if (!title || !description || !price || !thumbnail || !code || !stock) {
+//             console.log("Debe completar todos los Datos para Generar un nuevo Producto")
+//             return
+//         }
 
-        const codeExists = products.some(product => product.code === code)
-        if (codeExists) {
-            console.log("El Código del Producto ya existe. Debe ser único para cada Producto")
-            return
-        }
+//         const codeExists = products.some(product => product.code === code)
+//         if (codeExists) {
+//             console.log("El Código del Producto ya existe. Debe ser único para cada Producto")
+//             return
+//         }
 
 
-        if (products.length === 0) {
-            product.id = 1;
-        } else {
-            product.id = products[products.length - 1].id + 1;
-        }
+//         if (products.length === 0) {
+//             product.id = 1;
+//         } else {
+//             product.id = products[products.length - 1].id + 1;
+//         }
 
-        products.push(product)
-        await this.writeFile(products)
-        return this.getProducts()
+//         products.push(product)
+//         await this.writeFile(products)
+//         return this.getProducts()
 
-    }
+//     }
 
-    getProductById = async (id) => {
-        let products = await this.getProducts()
+//     getProductById = async (id) => {
+//         let products = await this.getProducts()
 
-        const productFind = products.find(product => product.id === id)
-        if (productFind) {
-            // console.log(productFind)
-            return productFind
-        } else {
-            console.log(`El producto con el ID: ${id} no ha sido encontrado`)
-            return
-        }
-    }
+//         const productFind = products.find(product => product.id === id)
+//         if (productFind) {
+//             // console.log(productFind)
+//             return productFind
+//         } else {
+//             console.log(`El producto con el ID: ${id} no ha sido encontrado`)
+//             return
+//         }
+//     }
 
-    updateProduct = async (id, { title, description, price, thumbnail, code, stock }) => {
-        try {
-            let products = await this.getProducts()
-            let product = await this.getProductById(id)
-            if (product) {
-                Object.assign(products[id - 1], { title, description, price, thumbnail, code, stock })
-                await this.writeFile(products)
-            }
-        } catch (error) {
-            console.log(error)
-        }
-    }
+//     updateProduct = async (id, { title, description, price, thumbnail, code, stock }) => {
+//         try {
+//             let products = await this.getProducts()
+//             let product = await this.getProductById(id)
+//             if (product) {
+//                 Object.assign(products[id - 1], { title, description, price, thumbnail, code, stock })
+//                 await this.writeFile(products)
+//             }
+//         } catch (error) {
+//             console.log(error)
+//         }
+//     }
 
-    deleteProduct = async (id) => {
-        try {
-            let products = await this.getProducts()
-            let product = await this.getProductById(id)
-            let newList = products.filter(prod => prod.id !== id)
-            if (product) {
-                await this.writeFile(newList)
-            }
+//     deleteProduct = async (id) => {
+//         try {
+//             let products = await this.getProducts()
+//             let product = await this.getProductById(id)
+//             let newList = products.filter(prod => prod.id !== id)
+//             if (product) {
+//                 await this.writeFile(newList)
+//             }
 
-        } catch (error) {
-            console.log(error)
-        }
-    }
+//         } catch (error) {
+//             console.log(error)
+//         }
+//     }
 
-}
+// }
 
-export default ProductManager
+// export default ProductManager
--- desafio3/app.js
+++ desafio3/app.js
@@ -1,42 +1,42 @@
-import express from 'express'
-import ProductManager from './ProductManager.js'
+// import express from 'express'
+// import ProductManager from './ProductManager.js'
 
-const productManager = new ProductManager('./src/products/products.json')
-let products = await productManager.getProducts()
+// const productManager = new ProductManager('./src/products/products.json')
+// let products = await productManager.getProducts()
 
-const app = express()
-app.use(express.urlencoded({ extended: true }))
+// const app = express()
+// app.use(express.urlencoded({ extended: true }))
 
-app.get("/", (req, res) => {
-    res.send("Servidor ON")
-})
+// app.get("/", (req, res) => {
+//     res.send("Servidor ON")
+// })
 
-app.get("/products", (req, res) => {
-    const { limit } = req.query
-    if (limit) {
-        const limitParse = parseInt(limit, 10)
-        const productsSlice = products.slice(0, limitParse)
-        res.send(productsSlice)
-    } else {
-        res.send(products)
-    }
+// app.get("/products", (req, res) => {
+//     const { limit } = req.query
+//     if (limit) {
+//         const limitParse = parseInt(limit, 10)
+//         const productsSlice = products.slice(0, limitParse)
+//         res.send(productsSlice)
+//     } else {
+//         res.send(products)
+//     }
 
-})
+// })
 
-app.get("/products/:pid", async (req, res) => {
-    try {
-        const idProduct = parseInt(req.params.pid, 10)
-        const productFind = await productManager.getProductById(idProduct)
-        if (!productFind) {
-            res.send(`No se encontró el Producto con el ID: ${idProduct}`)
-        }
-        res.send(productFind)
-    } catch (error) {
-        console.error(error)
-        res.status(500).send("Error interno del Servidor")
-    }
-})
+// app.get("/products/:pid", async (req, res) => {
+//     try {
+//         const idProduct = parseInt(req.params.pid, 10)
+//         const productFind = await productManager.getProductById(idProduct)
+//         if (!productFind) {
+//             res.send(`No se encontró el Producto con el ID: ${idProduct}`)
+//         }
+//         res.send(productFind)
+//     } catch (error) {
+//         console.error(error)
+//         res.status(500).send("Error interno del Servidor")
+//     }
+// })
 
-app.listen(8080, () => {
-    console.log("Servidor Inicializado en el Puerto 8080")
-})
+// app.listen(8080, () => {
+//     console.log("Servidor Inicializado en el Puerto 8080")
+// })
